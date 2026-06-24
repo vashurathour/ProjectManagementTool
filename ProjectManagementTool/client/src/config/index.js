a650c0eba30e477051395f54c3355dc7b76151f5
vashurathour <vashu2002rathour@gmail.com>
2026-06-24 11:04:02 +0530

    Update backend URL configuration

--- ProjectManagementTool/client/src/config/index.js
+++ ProjectManagementTool/client/src/config/index.js
@@ -1,6 +1,3 @@
 export const environment = process.env.NODE_ENV || "development";
 export const backendUrl =
-  // "https://methodic-backend.herokuapp.com/";
-  process.env.NODE_ENV === "production"
-    ? `https://methodic-backend.herokuapp.com/`
-    : "http://localhost:8080/";
+  process.env.REACT_APP_API_URL || "http://localhost:8080/";
