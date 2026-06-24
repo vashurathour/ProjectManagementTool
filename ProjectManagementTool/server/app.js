ec0222947762d564249cf79bb8d23ad71da5880d
vashurathour <vashu2002rathour@gmail.com>
2026-06-24 10:52:47 +0530

    Update route imports and API endpoints in app.js

--- ProjectManagementTool/server/app.js
+++ ProjectManagementTool/server/app.js
@@ -3,36 +3,30 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const { environment } = require("./config");
 const cors = require("cors");
-const userRouter = require("./routes/users");
-const taskRouter = require("./routes/tasks");
-const projectRouter = require("./routes/projects");
-const teamRouter = require("./routes/teams");
-const tasklistRouter = require("./routes/tasklists");
-const commentRouter = require("./routes/comments");
-const userteamRouter = require("./routes/userteams");
+const userRouter = require("./db/routes/users");
+const taskRouter = require("./db/routes/tasks");
+const projectRouter = require("./db/routes/projects");
+const teamRouter = require("./db/routes/teams");
+const tasklistRouter = require("./db/routes/tasklists");
+const commentRouter = require("./db/routes/comments");
+const userteamRouter = require("./db/routes/userteams");
 const app = express();
-
 app.use(bodyParser.json());
-
 // Same as bodyParser but built in
 // app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
-
 app.use(morgan("dev"));
 app.use(cors({ origin: true }));
-
 app.use(userRouter);
-app.use("./db/routes/task", taskRouter);
-app.use("./db/routes/project", projectRouter);
-app.use("./db/routes/team", teamRouter);
-app.use("./db/routes/tasklist", tasklistRouter);
-app.use("./db/routes/comment", commentRouter);
-app.use("./db/routes/userteam", userteamRouter);
-
+app.use("/api/tasks", taskRouter);
+app.use("/api/projects", projectRouter);
+app.use("/api/teams", teamRouter);
+app.use("/api/tasklists", tasklistRouter);
+app.use("/api/comments", commentRouter);
+app.use("/api/userteams", userteamRouter);
 app.get("/", (req, res) => {
   res.send("<h1>You're Connected </h1>");
 });
-
 // Catch unhandled requests such as wrong HTTP Method and forward to error handler.
 app.use((req, res, next) => {
   const err = new Error("The requested resource couldn't be found.");
@@ -40,7 +34,6 @@ app.use((req, res, next) => {
   err.errors = ["Could not find string of resource"];
   next(err);
 });
-
 // Generic error handler.
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
@@ -53,4 +46,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-module.exports = app;
+
